70c1af20d1fa7de2e5c513ef8995f55fe3c2ce52
Gigscourt <theprimestarventures@gmail.com>
2026-04-04 13:08:49 +0100

    Update splash.js

--- pages/splash.js
+++ pages/splash.js
@@ -1,1 +1,15 @@
+// pages/splash.js
+// Splash screen with 1.5 second fade out
 
+setTimeout(() => {
+    const container = document.querySelector('.splash-container');
+    if (container) {
+        container.style.opacity = '0';
+        container.style.transition = 'opacity 0.8s ease';
+        
+        setTimeout(() => {
+            // After fade, redirect to login
+            loadPage('login');
+        }, 800);
+    }
+}, 1500);
