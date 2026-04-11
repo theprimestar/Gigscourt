d9311c19b17a436c565dcf7c0014124c0d60bcf0
Gigscourt <theprimestarventures@gmail.com>
2026-04-11 15:16:04 +0100

    Update app-core.js

--- app-core.js
+++ app-core.js
@@ -10,7 +10,7 @@ import { getMessaging, getToken, onMessage } from 'https://www.gstatic.com/fireb
 import { getFirestore, doc, setDoc, getDoc, updateDoc, collection, addDoc, query, where, getDocs, orderBy, writeBatch, limit } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
 
 // Supabase configuration
-const supabaseUrl = 'https://qifzdrkpxzosdturjpex.supabase.co:6543';
+const supabaseUrl = 'https://qifzdrkpxzosdturjpex.supabase.co';
 const supabaseAnonKey = 'sb_publishable_QfKJ4jT8u_2HuUKmW-xvbQ_9acJvZw-';
 
 // ========== INITIALIZATION ==========
